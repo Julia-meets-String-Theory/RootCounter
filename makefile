9f25a8d3ef4ff3161e1f414053200d07911c4030
Martin Bies <MBies87@googlemail.com>
2023-01-02 14:04:09 +0100

    Move source code to src folder

--- makefile
+++ makefile
@@ -4,21 +4,21 @@
 all: install
 
 install: uninstall
-	(g++ -std=gnu++14 -c -lboost_thread -lboost_system -lpthread root_counter.cpp && g++ -std=gnu++14 -o rootCounter root_counter.o -lboost_thread -lboost_system -lpthread)
-	(g++ -std=c++17 root_counter_test.cpp -c -lboost_thread -lboost_system -lpthread -lcppunit -o rootCounterTest)
+	(cd src/ && g++ -std=gnu++14 -c -lboost_thread -lboost_system -lpthread root_counter.cpp && g++ -std=gnu++14 -o rootCounter root_counter.o -lboost_thread -lboost_system -lpthread)
+	(cd src/ && g++ -std=c++17 root_counter_test.cpp -lboost_thread -lboost_system -lpthread -lcppunit -o rootCounterTest)
 
 install-with-local-boost: uninstall \
 	install-boost-from-source \
-	(g++ -std=gnu++14 -c -Wl,-rpath=${P1} -I${P2} -L${P1} -lboost_thread -lboost_system -lpthread root_counter.cpp && g++ -std=gnu++14 -o rootCounter root_counter.o -Wl,-rpath=${P1} -I${P2} -L${P1} -lboost_thread -lboost_system -lpthread)
+	(cd src/ && g++ -std=gnu++14 -c -Wl,-rpath=${P1} -I${P2} -L${P1} -lboost_thread -lboost_system -lpthread root_counter.cpp && g++ -std=gnu++14 -o rootCounter root_counter.o -Wl,-rpath=${P1} -I${P2} -L${P1} -lboost_thread -lboost_system -lpthread)
 
 install-boost-from-source: uninstall-boost-from-source
-	(cd boost && wget -O boost_1_77_0.tar.gz https://sourceforge.net/projects/boost/files/boost/1.77.0/boost_1_77_0.tar.gz/download; tar xzvf boost_1_77_0.tar.gz; rm boost_1_77_0.tar.gz)
-	(cd boost && mkdir -p library)
-	(cd boost/boost_1_77_0 && ./bootstrap.sh --prefix=$$(realpath ../library) && ./b2 --prefix=$$(realpath ../library) --threading=multi install)
+	(cd src/boost && wget -O boost_1_77_0.tar.gz https://sourceforge.net/projects/boost/files/boost/1.77.0/boost_1_77_0.tar.gz/download; tar xzvf boost_1_77_0.tar.gz; rm boost_1_77_0.tar.gz)
+	(cd src/boost && mkdir -p library)
+	(cd src/boost/boost_1_77_0 && ./bootstrap.sh --prefix=$$(realpath ../library) && ./b2 --prefix=$$(realpath ../library) --threading=multi install)
 
 DIR := ${CURDIR}
-P1 := ${DIR}/boost/library/lib
-P2 := ${DIR}/boost/library/include
+P1 := ${DIR}/src/boost/library/lib
+P2 := ${DIR}/src/boost/library/include
 
 
 
@@ -27,8 +27,8 @@ P2 := ${DIR}/boost/library/include
 # Test the software
 
 test:
-	chmod +x rootCounterTest
-	./rootCounterTest
+	chmod +x src/rootCounterTest
+	src/./rootCounterTest
 
 
 
@@ -39,8 +39,7 @@ test:
 clean: uninstall
 
 uninstall: uninstall-boost-from-source
-	(rm -f root_counter.o && rm -f rootCounter)
-	($(RM) rootCounterTest)
+	(cd src/ && rm -f root_counter.o && rm -f rootCounter && rm -f rootCounterTest)
 
 uninstall-boost-from-source:
-	(cd boost && rm -r -f boost_1_77_0 && rm -r -f library)
+	(cd src/boost && rm -r -f boost_1_77_0 && rm -r -f library)
---
+++ src/root_counter_test.cpp
@@ -0,0 +1,114 @@
+// ####################
+// 1. Include libraries
+// 1. Include libraries
+// ####################
+
+// Include standard functionality
+#include <algorithm>
+#include <cassert>
+#include <chrono>
+#include <functional>
+#include <fstream>
+#include <iostream>
+#include <mutex>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <thread>
+#include <vector>
+
+// Include functionality of boost
+#include <boost/multiprecision/cpp_int.hpp>
+#include <boost/thread/mutex.hpp>
+#include <boost/thread/thread.hpp>
+#include <boost/multiprecision/cpp_bin_float.hpp>
+boost::mutex myGuard, myGuard2, myGuard3;
+
+// Include functionality of cppunit test
+#include <cppunit/TestRunner.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/BriefTestProgressListener.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+bool display_more_details = false;
+bool display_unsorted_setups = true;
+std::vector<std::vector<std::vector<int>>> unsorted;
+// Include specialized code that will be tested here
+#include "Auxilliary/print_vectors.cpp"
+#include "Auxilliary/handle_input.cpp"
+#include "Auxilliary/return_result.cpp"
+#include "Graphs/graphs.cpp"
+#include "Graphs/compute_graph_information.cpp"
+#include "H0/compute_h0_on_circuit.cpp"
+#include "H0/compute_h0_on_tree.cpp"
+#include "H0/compute_h0.cpp"
+#include "Combinatorics/combinatorics.cpp"
+#include "RootCounter/rootCounter-v3.cpp"
+#include "RootCounter/iterator.cpp"
+
+// Optimizations for speedup
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+
+
+
+// ####################
+// 2. Write tests
+// 2. Write tests
+// ####################
+
+class Test : public CPPUNIT_NS::TestCase
+{
+  CPPUNIT_TEST_SUITE(Test);
+  CPPUNIT_TEST(test1);
+  CPPUNIT_TEST_SUITE_END();
+
+public:
+  void setUp(void) {}
+  void tearDown(void) {}
+
+protected:
+  void test1(void) {
+    std::vector<std::vector<int>> edges = {{0,1},{0,2},{3,4},{3,4},{5,6},{6,7}};
+    if (3 != number_connected_components(edges)){
+      std::cout << "Wrong number of connected components found.\n";
+      std::cout << "Should be " << std::to_string(number_connected_components(edges)) << " connected components\n";
+      print_vector_of_vector("Edges\n", edges);
+      exit(1);
+    }
+  }
+};
+
+
+
+
+
+// ####################
+// 3. Execute tests
+// 3. Execute tests
+// ####################
+
+CPPUNIT_TEST_SUITE_REGISTRATION(Test);
+
+int main(){
+  // Create the event manager and test controller
+  CPPUNIT_NS::TestResult controller;
+
+  // Add a listener that colllects test result
+  CPPUNIT_NS::TestResultCollector result;
+  controller.addListener(&result);
+
+  // Add a listener that prints dots as test run.
+  CPPUNIT_NS::BriefTestProgressListener progress;
+  controller.addListener(&progress);
+
+  // Add the top suite to the test runner
+  CPPUNIT_NS::TestRunner runner;
+  runner.addTest(CPPUNIT_NS::TestFactoryRegistry::getRegistry().makeTest());
+  runner.run(controller);
+
+  // Return the result
+  return result.wasSuccessful() ? 0 : 1;
+}
