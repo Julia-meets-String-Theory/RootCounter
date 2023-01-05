21976e6849a78a3abfbddf5388441f96d7917d01
Martin Bies <MBies87@googlemail.com>
2023-01-05 13:33:26 +0100

    Remove function for sum of vector<vector<int>>

--- src/Auxilliary/sum_vectors.cpp
+++ src/Auxilliary/sum_vectors.cpp
@@ -8,12 +8,3 @@ boost::multiprecision::int128_t sum(const std::vector<std::vector<boost::multipr
     }
     return sum;
 }
-
-int sum(const std::vector<std::vector<int>> &values)
-{
-    int sum = 0;
-    for (int i = 0; i < values.size(); i++){
-        sum += std::accumulate(values[i].begin(), values[i].end(), 0);
-    }
-    return sum;
-}
