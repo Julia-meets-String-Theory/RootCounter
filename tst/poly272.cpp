98b9289a8aec4ed0ac9eab33e4add01ef73f8268
Martin Bies <MBies87@googlemail.com>
2023-01-10 14:22:26 +0100

    Fix typo

--- tst/poly272.cpp
+++ tst/poly272.cpp
@@ -17,7 +17,7 @@ int main(int argc, char* argv[]){
       {{683678800, 212949600, 29103200, 1932000, 43200, 0, 0, 0, 0, 0, 0}, {29538800, 10968000, 1224000, 0, 0, 0, 0, 0, 0, 0, 0}, {196800, 2000, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
   std::vector<std::vector<boost::multiprecision::int128_t>> n_lower_bound_expected =
       {{52464000, 172866400, 63572000, 11401200, 1061200, 49600, 2400, 0, 0, 0, 0}, {892000, 5600400, 2168800, 250800, 3600, 0, 0, 0, 0, 0, 0}, {0, 31200, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
-  assert(sum(n_exact) + sum(n_lower_bound) == sum(n_exact_expected) + sum(n_lower_bound_expected) && "Test for polytope 110 failed");
-  assert(n_exact == n_exact_expected && "Test for polytope 110 failed");
-  assert(n_lower_bound == n_lower_bound_expected && "Test for polytope 110 failed");
+  assert(sum(n_exact) + sum(n_lower_bound) == sum(n_exact_expected) + sum(n_lower_bound_expected) && "Test for polytope 272 failed");
+  assert(n_exact == n_exact_expected && "Test for polytope 272 failed");
+  assert(n_lower_bound == n_lower_bound_expected && "Test for polytope 272 failed");
 }
