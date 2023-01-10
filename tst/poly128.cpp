2ca9c42dcef47c89314bb500b7914b9e80db977a
Martin Bies <MBies87@googlemail.com>
2023-01-10 10:59:02 +0100

    Current tests always print out the result

--- tst/poly128.cpp
+++ tst/poly128.cpp
@@ -11,7 +11,8 @@
 
 int main(int argc, char* argv[]){
   std::vector<std::vector<boost::multiprecision::int128_t>> n_exact, n_lower_bound;
-  count_roots("6 12 12 12 12 12 12 0 0 0 0 0 0 9 3 0 5 0 5 3 0 4 2 3 1 5 2 4 1 4 1 2 4 12 8 3 3 0 9", n_exact, n_lower_bound);
+  std::vector<std::vector<std::vector<int>>> unsorted_setups;
+  count_roots("6 12 12 12 12 12 12 0 0 0 0 0 0 9 3 0 5 0 5 3 0 4 2 3 1 5 2 4 1 4 1 2 4 12 8 3 3 0 9", argv[0], true, n_exact, n_lower_bound, unsorted_setups);
   std::vector<std::vector<boost::multiprecision::int128_t>> n_exact_expected = {{8910, 9240, 1650, 814, 66, 33, 0, 0, 0, 0}};
   std::vector<std::vector<boost::multiprecision::int128_t>> n_lower_bound_expected = {{0, 0, 0, 0, 0, 0, 22, 0, 0, 1}};
   assert(sum(n_exact) + sum(n_lower_bound) == sum(n_exact_expected) + sum(n_lower_bound_expected) && "Test for polytope 128 failed");
--- tst/poly88.cpp
+++ tst/poly88.cpp
@@ -11,7 +11,8 @@
 
 int main(int argc, char* argv[]){
   std::vector<std::vector<boost::multiprecision::int128_t>> n_exact, n_lower_bound;
-  count_roots("5 16 80 32 16 16 0 1 0 0 0 9 4 0 0 3 2 3 2 4 0 1 1 4 1 3 1 2 1 2 6 20 8 3 5 0 9", n_exact, n_lower_bound);
+  std::vector<std::vector<std::vector<int>>> unsorted_setups;
+  count_roots("5 16 80 32 16 16 0 1 0 0 0 9 4 0 0 3 2 3 2 4 0 1 1 4 1 3 1 2 1 2 6 20 8 3 5 0 9", argv[0], true, n_exact, n_lower_bound, unsorted_setups);
   std::vector<std::vector<boost::multiprecision::int128_t>> n_exact_expected =
       {{756658800, 157590400, 12992800, 504800, 0, 0, 0, 0, 0, 0}, {25196800, 5967200, 399200, 0, 0, 0, 0, 0, 0, 0}, {106800, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
   std::vector<std::vector<boost::multiprecision::int128_t>> n_lower_bound_expected =
