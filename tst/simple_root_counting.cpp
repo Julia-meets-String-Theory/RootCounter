b90077b7ea1fca6f44b88f1a2b9a0d727bd26a9d
Martin Bies <MBies87@googlemail.com>
2023-01-24 16:58:34 +0100

    Add another simple root counting test

--- tst/simple_root_counting.cpp
+++ tst/simple_root_counting.cpp
@@ -42,6 +42,17 @@ void test3(const std::string & full_path){
   assert(n_lower_bound == n_lower_bound_expected && "Test for simple root counting 3 failed");
 }
 
+void test4(const std::string & full_path){
+  std::vector<std::vector<boost::multiprecision::int128_t>> n_exact, n_lower_bound;
+  std::vector<std::vector<std::vector<int>>> unsorted_setups;
+  count_roots("4 2 6 2 2 0 0 0 0 7 0 1 0 1 1 2 1 2 0 3 1 3 2 3 4 3 8 1 1 0 7", full_path, true, n_exact, n_lower_bound, unsorted_setups);
+  std::vector<std::vector<boost::multiprecision::int128_t>> n_exact_expected = {{5, 20, 15, 8, 0, 0, 0, 0}};
+  std::vector<std::vector<boost::multiprecision::int128_t>> n_lower_bound_expected = {{0, 0, 2, 14, 16, 1, 0, 0}};
+  assert(sum(n_exact) + sum(n_lower_bound) == sum(n_exact_expected) + sum(n_lower_bound_expected) && "Test for simple root counting 4 failed");
+  assert(n_exact == n_exact_expected && "Test for simple root counting 4 failed");
+  assert(n_lower_bound == n_lower_bound_expected && "Test for simple root counting 4 failed");
+}
+
 
 // ########################################
 // (2) Execute the tests
@@ -51,4 +62,5 @@ int main(int argc, char* argv[]){
   test1(argv[0]);
 	test2(argv[0]);
 	test3(argv[0]);
+	test4(argv[0]);
 }
