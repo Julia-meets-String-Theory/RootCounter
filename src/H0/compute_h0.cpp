809cb316b08f9eb172fe49a4531d0f337ca322fa
Martin Bies <MBies87@googlemail.com>
2023-02-01 13:49:19 +0100

    Rename nodal_edges to edges

--- src/H0/compute_h0.cpp
+++ src/H0/compute_h0.cpp
@@ -1,13 +1,13 @@
 // This method computes the exact number of global section on a CONNECTED nodal curve.
 // Should this not be possible (e.g. because of high genus components), then a lower bound is computed instead.
 int h0_on_connected_nodal_curve(const std::vector<int>& degrees,
-                                 const std::vector<std::vector<int>>& nodal_edges,
+                                 const std::vector<std::vector<int>>& edges,
                                  const std::vector<int> & genera,
                                  bool & lower_bound)
 {
     
     // (1) No edge, so must be a single component that is not self-connected.
-    if (nodal_edges.size() == 0 && degrees[0] >= 0){
+    if (edges.size() == 0 && degrees[0] >= 0){
         if (degrees[0] == 0 && genera[0] == 1){
             lower_bound = true;
         }
@@ -15,24 +15,24 @@ int h0_on_connected_nodal_curve(const std::vector<int>& degrees,
     }
     
     // (2) At least one edge, i.e. two components.
-    if (nodal_edges.size() > 0){
+    if (edges.size() > 0){
     
         // (2.1) Is there a non-rational component?
         bool rational = (*std::max_element(std::begin(genera), std::end(genera)) == 0);
         
         // (2.2) Compute h0 for a rational tree
-        if (betti_number(nodal_edges) == 0 && rational){
-            return h0_on_connected_rational_tree(degrees, nodal_edges);
+        if (betti_number(edges) == 0 && rational){
+            return h0_on_connected_rational_tree(degrees, edges);
         }
         
         // (2.3) Compute h0 for a rational bi-circuit
-        if (betti_number(nodal_edges) == 1 && rational && degrees.size() == 2 && nodal_edges[0][0] != nodal_edges[0][1] && nodal_edges[1][0] != nodal_edges[1][1]){
-            return h0_on_rational_bi_circuit(degrees, nodal_edges, lower_bound);
+        if (betti_number(edges) == 1 && rational && degrees.size() == 2 && edges[0][0] != edges[0][1] && edges[1][0] != edges[1][1]){
+            return h0_on_rational_bi_circuit(degrees, edges, lower_bound);
         }
         
         // (2.4) For all remaining cases, compute a lower bound
         lower_bound = true;
-        int number_nodes = nodal_edges.size();
+        int number_nodes = edges.size();
         int local_sections = 0;
         for (int i = 0; i < degrees.size(); i++){
             if (degrees[i] >= 0){
@@ -56,7 +56,7 @@ int h0_on_connected_nodal_curve(const std::vector<int>& degrees,
 // This method will modify the referenced boolean lower_bound to be true if we merely computed a lower bound.
 // Otherwise, it will be set to be false.
 int h0_on_nodal_curve(const std::vector<int>& degrees,
-                        const std::vector<std::vector<int>>& nodal_edges,
+                        const std::vector<std::vector<int>>& edges,
                         const std::vector<int> & genera,
                         bool & lower_bound)
 {
@@ -67,7 +67,7 @@ int h0_on_nodal_curve(const std::vector<int>& degrees,
     // (1) Compute the connected components
     std::vector<std::vector<std::vector<int>>> edges_of_cc;
     std::vector<std::vector<int>> degs_of_cc, gens_of_cc;
-    find_connected_components(nodal_edges, degrees, genera, edges_of_cc, degs_of_cc, gens_of_cc);
+    find_connected_components(edges, degrees, genera, edges_of_cc, degs_of_cc, gens_of_cc);
     
     // (2) Compute h0 by iterating over the connected components
     int h0 = 0;
