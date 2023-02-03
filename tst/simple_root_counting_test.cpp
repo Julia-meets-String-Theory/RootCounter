6219b3a4b9ab6d42493b7bc205108b880493370e
Martin Bies <MBies87@googlemail.com>
2023-02-03 16:03:43 +0100

    Add functionality for rational double-loop with 4 edges and 3 vertices

--- tst/simple_root_counting_test.cpp
+++ tst/simple_root_counting_test.cpp
@@ -46,8 +46,8 @@ void test3(const std::string & full_path){
   std::vector<std::vector<boost::multiprecision::int128_t>> n_exact, n_lower_bound;
   std::vector<std::vector<std::vector<int>>> unsorted_setups;
   count_roots("3 1 2 1 0 0 0 5 0 1 0 1 1 2 1 2 0 2 3 2 8 0 1 0 5", full_path, true, n_exact, n_lower_bound, unsorted_setups);
-  std::vector<std::vector<boost::multiprecision::int128_t>> n_exact_expected = {{1, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}};
-  std::vector<std::vector<boost::multiprecision::int128_t>> n_lower_bound_expected = {{0, 0, 2, 4, 1, 0}, {0, 0, 0, 0, 0, 0}};
+  std::vector<std::vector<boost::multiprecision::int128_t>> n_exact_expected = {{1, 0, 0, 0, 1, 0}, {0, 0, 0, 0, 0, 0}};
+  std::vector<std::vector<boost::multiprecision::int128_t>> n_lower_bound_expected = {{0, 0, 2, 4, 0, 0}, {0, 0, 0, 0, 0, 0}};
   assert(sum(n_exact) + sum(n_lower_bound) == sum(n_exact_expected) + sum(n_lower_bound_expected) && "Test for simple root counting 3 failed");
   assert(n_exact == n_exact_expected && "Test for simple root counting 3 failed");
   assert(n_lower_bound == n_lower_bound_expected && "Test for simple root counting 3 failed");
