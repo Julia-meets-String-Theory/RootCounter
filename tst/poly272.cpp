c9b7b5b3a230eb0c1744527eecdb34f5c0c32c3d
Martin Bies <MBies87@googlemail.com>
2023-02-15 22:50:12 +0100

    Print computed results to console even if tests fail

--- tst/poly272.cpp
+++ tst/poly272.cpp
@@ -17,7 +17,7 @@ int main(int argc, char* argv[]){
       {{736011640, 229717974, 31225880, 2127265, 47600, 0, 0, 0, 0, 0, 0}, {30428570, 11338671, 1224000, 0, 0, 0, 0, 0, 0, 0, 0}, {196800, 2000, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
   std::vector<std::vector<boost::multiprecision::int128_t>> n_lower_bound_expected =
       {{131160, 156098026, 61449320, 11205935, 1056800, 49600, 2400, 0, 0, 0, 0}, {2230, 5229729, 2168800, 250800, 3600, 0, 0, 0, 0, 0, 0}, {0, 31200, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
-  assert(sum(n_exact) + sum(n_lower_bound) == sum(n_exact_expected) + sum(n_lower_bound_expected) && "Test for polytope 272 failed");
-  assert(n_exact == n_exact_expected && "Test for polytope 272 failed");
-  assert(n_lower_bound == n_lower_bound_expected && "Test for polytope 272 failed");
+  if ((sum(n_exact) + sum(n_lower_bound) != sum(n_exact_expected) + sum(n_lower_bound_expected)) || (n_exact != n_exact_expected) || (n_lower_bound != n_lower_bound_expected)){
+    return -1;
+  }
 }
--- tst/poly786.cpp
+++ tst/poly786.cpp
@@ -17,7 +17,7 @@ int main(int argc, char* argv[]){
   {{32860461, 20719735, 6053632, 1080597, 120420, 8175, 438, 14, 0, 0, 0, 0, 0}, {1719897, 1076991, 269709, 33335, 1821, 30, 0, 0, 0, 0, 0, 0, 0}, {19661, 6835, 680, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
   std::vector<std::vector<boost::multiprecision::int128_t>> n_lower_bound_expected =
     {{0, 0, 14120, 8975, 3326, 800, 131, 21, 0, 0, 0, 0, 0}, {0, 0, 134, 13, 18, 6, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
-  assert(sum(n_exact) + sum(n_lower_bound) == sum(n_exact_expected) + sum(n_lower_bound_expected) && "Test for polytope 798 failed");
-  assert(n_exact == n_exact_expected && "Test for polytope 798 failed");
-  assert(n_lower_bound == n_lower_bound_expected && "Test for polytope 798 failed");
+  if ((sum(n_exact) + sum(n_lower_bound) != sum(n_exact_expected) + sum(n_lower_bound_expected)) || (n_exact != n_exact_expected) || (n_lower_bound != n_lower_bound_expected)){
+    return -1;
+  }
 }
