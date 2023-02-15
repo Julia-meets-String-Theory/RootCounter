21ee5342cdb8920bb3ba6ad79a59c50fd114ba16
Martin Bies <MBies87@googlemail.com>
2023-02-15 22:40:15 +0100

    Add proper test results for polytope 762

--- tst/poly762.cpp
+++ tst/poly762.cpp
@@ -13,10 +13,10 @@ int main(int argc, char* argv[]){
   std::vector<std::vector<boost::multiprecision::int128_t>> n_exact, n_lower_bound;
   std::vector<std::vector<std::vector<int>>> unsorted_setups;
   count_roots("7 16 16 32 32 32 16 16 0 0 0 0 0 0 0 12 1 0 3 2 3 2 4 0 4 1 4 2 4 3 6 0 5 1 5 6 3 6 2 5 6 20 8 3 5 0 12", argv[0], true, n_exact, n_lower_bound, unsorted_setups);
-  std::vector<std::vector<boost::multiprecision::int128_t>> n_exact_expected 
-      {{690950608, 250171050, 40113524, 3586377, 157600, 2400, 0, 0, 0, 0, 0, 0}, {37074570, 16720271, 2840800, 147600, 0, 0, 0, 0, 0, 0, 0, 0}, {469600, 128800, 400, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+  std::vector<std::vector<boost::multiprecision::int128_t>> n_exact_expected =
+  {{32858151, 20703000, 6040190, 1077204, 121534, 8446, 314, 12, 0, 0, 0, 0, 0}, {1722871, 1093360, 285368, 35811, 1881, 30, 0, 0, 0, 0, 0, 0, 0}, {18984, 6702, 280, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
   std::vector<std::vector<boost::multiprecision::int128_t>> n_lower_bound_expected =
-      {{123792, 146465350, 65512476, 13576423, 1494400, 80800, 6400, 0, 0, 0, 0, 0},{2230, 6645729, 3139200, 513600, 12000, 0, 0, 0, 0, 0, 0, 0},{0, 54400, 9600, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+   {{0, 0, 14102, 7428, 3091, 758, 104, 6, 0, 0, 0, 0, 0}, {0, 0, 242, 107, 24, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
   assert(sum(n_exact) + sum(n_lower_bound) == sum(n_exact_expected) + sum(n_lower_bound_expected) && "Test for polytope 798 failed");
   assert(n_exact == n_exact_expected && "Test for polytope 798 failed");
   assert(n_lower_bound == n_lower_bound_expected && "Test for polytope 798 failed");
