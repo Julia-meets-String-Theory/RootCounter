19766bff5fc4b4d33ca3481937b78a57955457e3
Martin Bies <MBies87@googlemail.com>
2023-02-16 23:58:11 +0100

    Fix minor issue in some test files

--- tst/poly1879.cpp
+++ tst/poly1879.cpp
@@ -14,7 +14,7 @@ int main(int argc, char* argv[]){
   std::vector<std::vector<std::vector<int>>> unsorted_setups;
   count_roots(
     "9 16 16 16 16 32 16 16 16 16 0 0 0 0 0 0 0 0 0 14 2 1 4 1 4 2 5 3 5 4 6 0 6 4 6 5 3 0 0 8 3 7 7 8 2 8 1 7 6 20 8 3 6 0 14", argv[0], true, n_exact, n_lower_bound, unsorted_setups);
-  std::vector<std::vector<boost::multiprecision::int128_t>> n_exact_expected 
+  std::vector<std::vector<boost::multiprecision::int128_t>> n_exact_expected =
       {{690950608, 250171050, 40113524, 3586377, 157600, 2400, 0, 0, 0, 0, 0, 0}, {37074570, 16720271, 2840800, 147600, 0, 0, 0, 0, 0, 0, 0, 0}, {469600, 128800, 400, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
   std::vector<std::vector<boost::multiprecision::int128_t>> n_lower_bound_expected =
       {{123792, 146465350, 65512476, 13576423, 1494400, 80800, 6400, 0, 0, 0, 0, 0},{2230, 6645729, 3139200, 513600, 12000, 0, 0, 0, 0, 0, 0, 0},{0, 54400, 9600, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
