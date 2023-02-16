be7c3897b9d929b9d4bd936745274b131507d89d
Martin Bies <MBies87@googlemail.com>
2023-02-16 11:11:50 +0100

    Add proper results for poly838

--- tst/poly838.cpp
+++ tst/poly838.cpp
@@ -13,10 +13,10 @@ int main(int argc, char* argv[]){
   std::vector<std::vector<boost::multiprecision::int128_t>> n_exact, n_lower_bound;
   std::vector<std::vector<std::vector<int>>> unsorted_setups;
   count_roots("7 16 48 32 16 16 16 16 0 0 0 0 0 0 0 12 2 1 2 1 5 0 4 0 5 6 3 4 3 6 0 2 2 4 1 5 1 6 1 3 6 20 8 3 5 0 12", argv[0], true, n_exact, n_lower_bound, unsorted_setups);
-  std::vector<std::vector<boost::multiprecision::int128_t>> n_exact_expected 
-      {{690950608, 250171050, 40113524, 3586377, 157600, 2400, 0, 0, 0, 0, 0, 0}, {37074570, 16720271, 2840800, 147600, 0, 0, 0, 0, 0, 0, 0, 0}, {469600, 128800, 400, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+  std::vector<std::vector<boost::multiprecision::int128_t>> n_exact_expected =
+      {{32845047, 20641615, 6048558, 1075389, 120778, 7966, 401, 16, 0, 0, 0, 0, 0}, {1739918, 1137880, 295128, 38834, 1989, 27, 0, 0, 0, 0, 0, 0, 0}, {20615, 9155, 717, 13, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
   std::vector<std::vector<boost::multiprecision::int128_t>> n_lower_bound_expected =
-      {{123792, 146465350, 65512476, 13576423, 1494400, 80800, 6400, 0, 0, 0, 0, 0},{2230, 6645729, 3139200, 513600, 12000, 0, 0, 0, 0, 0, 0, 0},{0, 54400, 9600, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+      {{0, 0, 5817, 6791, 2512, 677, 99, 14, 0, 0, 0, 0, 0}, {0, 0, 0, 23, 21, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
   if ((sum(n_exact) + sum(n_lower_bound) != sum(n_exact_expected) + sum(n_lower_bound_expected)) || (n_exact != n_exact_expected) || (n_lower_bound != n_lower_bound_expected)){
     return -1;
   }
