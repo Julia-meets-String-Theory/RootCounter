115bb5957183b51c4ae5fbe8c2b691cd759c27d5
Martin Bies <MBies87@googlemail.com>
2023-02-17 13:56:15 +0100

    Speed-up tests by limiting the number of blowups to be considered

--- tst/poly52.cpp
+++ tst/poly52.cpp
@@ -12,11 +12,11 @@
 int main(int argc, char* argv[]){
   std::vector<std::vector<boost::multiprecision::int128_t>> n_exact, n_lower_bound;
   std::vector<std::vector<std::vector<int>>> unsorted_setups;
-  count_roots("6 16 48 16 48 16 16 0 0 0 0 0 0 11 2 0 2 1 5 0 5 2 5 3 4 0 3 4 1 4 1 3 1 3 1 3 6 20 8 3 5 0 11", argv[0], true, n_exact, n_lower_bound, unsorted_setups);
+  count_roots("6 16 48 16 48 16 16 0 0 0 0 0 0 11 2 0 2 1 5 0 5 2 5 3 4 0 3 4 1 4 1 3 1 3 1 3 6 20 8 3 5 0 7", argv[0], true, n_exact, n_lower_bound, unsorted_setups);
   std::vector<std::vector<boost::multiprecision::int128_t>> n_exact_expected =
-      {{34980351, 20149232, 5402789, 856967, 83265, 4851, 116, 4, 0, 0, 0, 0}, {1438026, 862358, 183547, 15688, 597, 9, 0, 0, 0, 0, 0, 0}, {9794, 1408, 54, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+      {{34980351, 20149232, 5402789, 856967, 83265, 4851, 116, 4}, {1438026, 862358, 183547, 15688, 597, 9, 0, 0}, {9794, 1408, 54, 0, 0, 0, 0, 0}};
   std::vector<std::vector<boost::multiprecision::int128_t>> n_lower_bound_expected =
-      {{0, 0, 51, 8160, 2423, 274, 11, 0, 0, 0, 0, 0}, {0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+      {{0, 0, 51, 8160, 2423, 274, 11, 0}, {0, 0, 0, 25, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0}};
   if ((sum(n_exact) + sum(n_lower_bound) != sum(n_exact_expected) + sum(n_lower_bound_expected)) || (n_exact != n_exact_expected) || (n_lower_bound != n_lower_bound_expected)){
     return -1;
   }
--- tst/poly762.cpp
+++ tst/poly762.cpp
@@ -12,11 +12,11 @@
 int main(int argc, char* argv[]){
   std::vector<std::vector<boost::multiprecision::int128_t>> n_exact, n_lower_bound;
   std::vector<std::vector<std::vector<int>>> unsorted_setups;
-  count_roots("7 16 16 32 32 32 16 16 0 0 0 0 0 0 0 12 1 0 3 2 3 2 4 0 4 1 4 2 4 3 6 0 5 1 5 6 3 6 2 5 6 20 8 3 5 0 12", argv[0], true, n_exact, n_lower_bound, unsorted_setups);
+  count_roots("7 16 16 32 32 32 16 16 0 0 0 0 0 0 0 12 1 0 3 2 3 2 4 0 4 1 4 2 4 3 6 0 5 1 5 6 3 6 2 5 6 20 8 3 5 0 7", argv[0], true, n_exact, n_lower_bound, unsorted_setups);
   std::vector<std::vector<boost::multiprecision::int128_t>> n_exact_expected =
-      {{32858151, 20703000, 6040190, 1077204, 122758, 8972, 406, 18, 0, 0, 0, 0, 0}, {1722871, 1093360, 285368, 35811, 1905, 30, 0, 0, 0, 0, 0, 0, 0}, {18984, 6702, 280, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+      {{32858151, 20703000, 6040190, 1077204, 122758, 8972, 406, 18}, {1722871, 1093360, 285368, 35811, 1905, 30, 0, 0}, {18984, 6702, 280, 0, 0, 0, 0, 0}};
   std::vector<std::vector<boost::multiprecision::int128_t>> n_lower_bound_expected =
-      {{0, 0, 14102, 7428, 1867, 232, 12, 0, 0, 0, 0, 0, 0}, {0, 0, 242, 107, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+      {{0, 0, 14102, 7428, 1867, 232, 12, 0}, {0, 0, 242, 107, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0}};
    if ((sum(n_exact) + sum(n_lower_bound) != sum(n_exact_expected) + sum(n_lower_bound_expected)) || (n_exact != n_exact_expected) || (n_lower_bound != n_lower_bound_expected)){
     return -1;
   }
