5d4af45cf87a974a9c430b1a75dca763b84dceb2
Martin Bies <MBies87@googlemail.com>
2023-02-20 10:22:20 +0100

    Disable tests for polytopes that take too long

--- tst/poly1384.cpp
+++ tst/poly1384.cpp
@@ -1,10 +1,10 @@
 #include "include.cpp"
 
 int main(int argc, char* argv[]){
-  std::string input = "10 16 16 16 16 16 16 16 16 16 16 0 0 0 0 0 0 0 0 0 0 15 1 0 2 0 4 1 5 0 6 1 6 5 8 6 9 5 8 9 7 9 3 8 3 7 2 7 3 4 2 4 6 20 8 3 6 0 7";
+  /*std::string input = "10 16 16 16 16 16 16 16 16 16 16 0 0 0 0 0 0 0 0 0 0 15 1 0 2 0 4 1 5 0 6 1 6 5 8 6 9 5 8 9 7 9 3 8 3 7 2 7 3 4 2 4 6 20 8 3 6 0 7";
   std::vector<std::vector<boost::multiprecision::int128_t>> n_exact_expected =
       {{27178020, 21092430, 7757630, 1806325, 280930, 27970, 2060, 200}, {2439920, 2184225, 870520, 193330, 21060, 930, 0, 0}, {68270, 56190, 15010, 2170, 0, 0, 0, 0}, {540, 230, 0, 0, 0, 0, 0, 0}};
   std::vector<std::vector<boost::multiprecision::int128_t>> n_lower_bound_expected =
       {{0, 0, 0, 0, 1330, 610, 40, 15}, {0, 0, 0, 0, 30, 15, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0}};
-  return conduct_test(input, argv[0], n_exact_expected, n_lower_bound_expected, (boost::multiprecision::int128_t) (pow(20, 6)));
+  return conduct_test(input, argv[0], n_exact_expected, n_lower_bound_expected, (boost::multiprecision::int128_t) (pow(20, 6)));*/
 }
